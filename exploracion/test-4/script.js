4712f5d71c7b686c5656c84aa18f5784801dbf34
FedeeGiorgi <fgiorgi@mail.utdt.edu>
2023-04-04 19:51:08 -0300

    borro las exp que no funcionaban x los counts

--- exploracion/test-4/script.js
+++ exploracion/test-4/script.js
@@ -1,16 +1,14 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
   // Guardamos el svg generado en la variable chart
-  let chart =   Plot.plot({
+  let chart = Plot.plot({
     marks: [
-      Plot.barY(data, {
-        x: 'anio_mision',
-        y: 'cantidad_astronautas_anio', // No se calcular esto
-        fill: 'genero',
+      Plot.dot(data, {
+        x: 'status', 
+        y: 'mision_hs',
+        stroke: 'genero'
       }),
     ],
-    marginLeft: 70,
-    width: 300,
   })
   // Agregamos chart al div#chart de index.html
   d3.select('#chart').append(() => chart)
--- exploracion/test-5/script.js
+++ exploracion/test-5/script.js
@@ -1,26 +1,23 @@
-d3.csv('astronautas.csv', d3.autoType).then(data => {
+d3.csv("astronautas.csv", d3.autoType).then((data) => {
   let chart = Plot.plot({
     marks: [
-      Plot.barY(data, {
-        x: 'genero',
-        y: 'mision_hs',
-        fill: 'genero',
-        order: 'fill'
-      })
+      Plot.barX(data, {
+         x: "eva_mision_hs", 
+         y: "nacionalidad", 
+         fill: "nacionalidad"
+         })
     ],
     width: 600,
     height: 400,
     margin: 80,
     inset: 10,
-    x: {
-      label: 'Genero'
-    },
-    y: {
-      label: 'Duración total de misiones (horas)'
-    },
-    color: {
-        range: ['#FF69b4', 'blue'],
+    nice: true,
+    line: true,
+    grid: true,
+    zero: true,
+    color:{
+      range : ["#F94144", "#F3722C", "#F8961E", "#F9C74F", "#90BE6D", "#43AA8B", "#4D908E", "#577590", "#277DA1", "#3B3B98", "#5E548E", "#8D5B4C", "#333333"],
     }
-  })
-  d3.select('#chart').append(() => chart)
-})
+  });
+  d3.select("#chart").append(() => chart);
+});
